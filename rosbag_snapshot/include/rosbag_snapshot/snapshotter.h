2f840c65fb3130bdb359eb98a4c4314595c38391
Linwei Zhuo <53355440+DiuLaMaX@users.noreply.github.com>
2023-08-15 07:52:06 -0700

    added option to clear buffer on resuming the buffer. Enables multiple… (#26)
    * added option to clear buffer on resuming the buffer. Enables multiple calls to write bags in a succession for different groupings in rosbag files
    
    * formatting
    
    ---------
    
    Co-authored-by: David Tarazi <dtarazi@olin.edu>
    Co-authored-by: David V. Lu!! <davidvlu@gmail.com>

--- rosbag_snapshot/include/rosbag_snapshot/snapshotter.h
+++ rosbag_snapshot/include/rosbag_snapshot/snapshotter.h
@@ -101,6 +101,8 @@ struct ROSBAG_DECL SnapshotterOptions
   ros::Duration status_period_;
   // Flag if all topics should be recorded
   bool all_topics_;
+  // Flag for clearing buffers or not after writing to bag
+  bool clear_buffer_;
   // Compression type
   std::string compression_;
 
@@ -109,7 +111,8 @@ struct ROSBAG_DECL SnapshotterOptions
   topics_t topics_;
 
   SnapshotterOptions(ros::Duration default_duration_limit = ros::Duration(30), int32_t default_memory_limit = -1,
-                     int32_t default_count_limit = -1, ros::Duration status_period = ros::Duration(1));
+                     int32_t default_count_limit = -1, ros::Duration status_period = ros::Duration(1),
+                     bool clear_buffer = true);
 
   // Add a new topic to the configuration, returns false if the topic was already present
   bool addTopic(std::string const& topic,
